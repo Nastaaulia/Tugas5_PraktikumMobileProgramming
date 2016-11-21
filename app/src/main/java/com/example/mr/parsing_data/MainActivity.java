f8a60ca48f91074d4de7f28df3ab536f1964d010
Nastaaulia <nasta@student.uinsgd.ac.id>
2016-11-21 16:53:55 +0700

    Tugas 5 Praktikum Mobpro

---
+++ app/src/main/java/com/example/mr/parsing_data/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.mr.parsing_data;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+public class MainActivity extends ListActivity {
+    // URL to get contacts JSON
+    private static String url = "http://dthan.net/json_mhs.php?nim=1137050164";
+    // JSON Node names
+    private static final String TAG_STUDENTINFO = "data";
+    private static final String TAG_NIM = "nim";
+    private static final String TAG_NAMA = "nm_pd";
+    private static final String TAG_ANGKATAN = "angkatan";
+    private static final String TAG_ALAMAT = "jln";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new GetStudents().execute();
+    }
+    /**
+     * Async task class to get json by making HTTP call
+     */
+    private class GetStudents extends AsyncTask<Void, Void, Void> {
+        // Hashmap for ListView
+        ArrayList<HashMap<String, String>> studentList;
+        ProgressDialog pDialog;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+// Showing progress dialog
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+        @Override
+        protected Void doInBackground(Void... arg0) {
+// Creating service handler class instance
+            koneksi webreq = new koneksi();
+// Making a request to url and getting response
+            String jsonStr = webreq.makeWebServiceCall(url, koneksi.GET);
+            Log.d("Response: ", "> " + jsonStr);
+            studentList = ParseJSON(jsonStr);
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+// Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+/**
+ * Updating parsed JSON data into ListView
+ * */
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, studentList,
+                    R.layout.list_item, new String[]{TAG_NAMA, TAG_NIM,
+                    TAG_ANGKATAN,TAG_ALAMAT}, new int[]{R.id.nama,
+                    R.id.nim, R.id.angkatan,R.id.alamat});
+            setListAdapter(adapter);
+        }
+    }
+    private ArrayList<HashMap<String, String>> ParseJSON(String json) {
+        if (json != null) {
+            try {
+// Hashmap for ListView
+                ArrayList<HashMap<String, String>> studentList = new
+                        ArrayList<HashMap<String, String>>();
+                JSONObject jsonObj = new JSONObject(json);
+                JSONArray students = jsonObj.getJSONArray(TAG_STUDENTINFO);
+// looping through All Students
+                for (int i = 0; i < students.length(); i++) {
+                    JSONObject c = students.getJSONObject(i);
+                    String nama = "Nama : " + c.getString(TAG_NAMA);
+                    String nim = "NIM : " + c.getString(TAG_NIM);
+                    String angkatan = "Angkatan : " + c.getString(TAG_ANGKATAN);
+                    String jln = "jalan : " + c.getString(TAG_ALAMAT);
+
+// tmp hashmap for single student
+                    HashMap<String, String> student = new HashMap<String,
+                            String>();
+// adding each child node to HashMap key => value
+                    student.put(TAG_NAMA, nama);
+                    student.put(TAG_NIM, nim);
+                    student.put(TAG_ANGKATAN, angkatan);
+                    student.put(TAG_ALAMAT, jln);
+// adding student to students list
+                    studentList.add(student);
+                }
+                return studentList;
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+        } else {
+            Log.e("ServiceHandler", "Couldn't get any data from the url");
+            return null;
+        }
+    }
+}
